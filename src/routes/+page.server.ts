cdb21f216711c01b71a70388372467b6ff4c3ba1
brittneypostma <bdesignedwebdev@gmail.com>
2022-11-01 15:03:26 -0400

    cleaned up server code

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -13,9 +13,9 @@ export const load: PageServerLoad = async (event: RequestEvent) => {
 		// location = await iplocation(context.ip);
 		locationLabel = `${context.geo.city}, ${context.geo.country.name}`;
 	} catch (err) {
-		throw error(404, `${err.message}`);
-		// location = null;
-		// locationLabel = 'London, England';
+		location = null;
+		locationLabel = undefined;
+		throw error(404, `Error: ${err.message}`);
 	}
 
 	const locale = location?.country?.languages[0] || 'en-GB';
@@ -29,16 +29,6 @@ export const load: PageServerLoad = async (event: RequestEvent) => {
 		minute: 'numeric'
 	});
 
-	// Get the page content
-	const response = await context.next();
-	const page = await response.text();
-
-	// Replace the content
-	const regex_time = /CURRENT_TIME/gi;
-	const regex_place = /CURRENT_LOCATION/gi;
-	// let updatedPage = page.replace(regex_time, time);
-	// updatedPage = updatedPage.replace(regex_place, locationLabel);
-	// return new Response(updatedPage, response);
 	return {
 		locationLabel,
 		time
