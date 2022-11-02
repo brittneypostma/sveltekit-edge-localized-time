4b2c1995aaa337587c2f8bcb3f7dc64f360fb542
brittneypostma <bdesignedwebdev@gmail.com>
2022-11-02 08:49:48 -0400

    location

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,29 +5,29 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event: RequestEvent) => {
 	const context: Context = event.platform?.context;
-	let location = event.platform?.context?.geo?.ip;
+	let location;
 	let locationLabel;
 	try {
+		location = event.platform?.context?.geo?.ip;
 		locationLabel = `${context.geo.city}, ${context.geo.country.name}`;
+
+		const locale = location?.country?.languages[0] || 'en-GB';
+		const timezone = location?.country?.timezone?.code || 'Europe/London'; //  'America/New_York'
+		console.log(location, location?.country?.languages[0], location?.country?.timezone?.code);
+
+		// Generate a formatted time string
+		const time = new Date().toLocaleString(locale, {
+			timeZone: timezone,
+			hour: 'numeric',
+			minute: 'numeric'
+		});
+		return {
+			locationLabel,
+			time
+		};
 	} catch (err) {
 		location = null;
 		locationLabel = undefined;
 		throw error(404, `Error: ${err.message}`);
 	}
-
-	const locale = location?.country?.languages[0] || 'en-GB';
-	const timezone = location?.country?.timezone?.code || 'Europe/Londom'; //  'America/New_York'
-	console.log(locale, timezone);
-
-	// Generate a formatted time string
-	const time = new Date().toLocaleString(locale, {
-		timeZone: timezone,
-		hour: 'numeric',
-		minute: 'numeric'
-	});
-
-	return {
-		locationLabel,
-		time
-	};
 };
